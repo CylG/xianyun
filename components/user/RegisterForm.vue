998062dd0d168e62b0b6b48149b55c8ad8b583c7
Cyl <1611360607@qq.com>
2019-06-30 23:51:03 +0800

    3.登录注册

--- components/user/RegisterForm.vue
+++ components/user/RegisterForm.vue
@@ -1,129 +1,114 @@
 <template>
     <el-form 
         :model="form" 
-        ref="form"
+        ref="form" 
         :rules="rules" 
         class="form">
+            <el-form-item class="form-item">
+                <el-input 
+                placeholder="用户名手机"
+                v-model="form.username">
+                </el-input>
+            </el-form-item>
 
-        <!-- 新增了prop属性 -->
-        <el-form-item class="form-item" prop="userphone">
-            <el-input 
-                    placeholder="用户名手机"
-                    v-model="form.userphone">
-            </el-input>
-        </el-form-item>
-        <!-- 新增了prop属性 -->
-        <el-form-item class="form-item" prop="num">
-            <el-input 
-                    placeholder="验证码"
-                    v-model="form.num">
+            <el-form-item class="form-item">
+                <el-input 
+                placeholder="验证码"
+                v-model="form.captcha" >
                     <template slot="append">
-                        <el-button @click="handleSendNum">
+                        <el-button @click="handleSendCaptcha">
                             发送验证码
                         </el-button>
                     </template>
-            </el-input>
-        </el-form-item>
-        <!-- 新增了prop属性 -->
-        <el-form-item class="form-item" prop="username">
-            <el-input 
-                    placeholder="你的名字"
-                    v-model="form.username">
-            </el-input>
-        </el-form-item>
-        <!-- 新增了prop属性 -->
-        <el-form-item class="form-item" prop="password">
-            <el-input 
-                    placeholder="密码" 
-                    type="password"
-                    v-model="form.num">
-            </el-input>
-        </el-form-item>
-        <!-- 新增了prop属性 -->
-        <el-form-item class="form-item" prop="confirmpassword">
-            <el-input 
-                    placeholder="确认密码" 
-                    type="password"
-                    v-model="form.confirmpassword">
-            </el-input>
-        </el-form-item>
+                </el-input>
+            </el-form-item>
 
-        <el-button 
-        class="submit"
-        type="primary"
-        @click="handleRegisterSubmit">
-            注册
-        </el-button>
-    </el-form>
+            <el-form-item class="form-item">
+                <el-input 
+                placeholder="你的名字"
+                v-model="form.nickname">
+                </el-input>
+            </el-form-item>
 
+            <el-form-item class="form-item">
+                <el-input 
+                placeholder="密码" 
+                type="password"
+                v-model="form.password"
+                ></el-input>
+            </el-form-item>
+
+            <el-form-item class="form-item">
+                <el-input 
+                placeholder="确认密码" 
+                type="password"
+                v-model="form.checkPassword">
+                </el-input>
+            </el-form-item>
+
+            <el-button 
+            class="submit" 
+            type="primary" 
+            @click="handleRegSubmit">
+                注册
+            </el-button>
+        </el-form>
 </template>
 
 <script>
 export default {
-        data(){
-             // 确认密码
-            const validatePass = (rule, value, callback) => {
-                if (value === '') {
-                    callback(new Error('请再次输入密码'));
-                } else if (value !== this.form.password) {
-                    callback(new Error('两次输入密码不一致!'));
-                } else {
-                    callback();
-                }
+    data(){
+         // 确认密码
+        const validatePass = (rule, value, callback) => {
+            if (value === '') {
+                callback(new Error('请再次输入密码'));
+            } else if (value !== this.form.password) {
+                callback(new Error('两次输入密码不一致!'));
+            } else {
+                callback();
             }
-            return {
-                // 表单数据
-                form: {
-                    userphone: "",          // 注册用户名手机
-                    num: "",                // 注册验证码
-                    username: "",           // 注册用户名字
-                    password: "",           // 注册密码
-                    confirmpassword: ""     // 注册再次确认密码
-                },
-                // 表单规则
-                rules: {
-                    userphone: [
-                        { 
-                            required: true, 
-                            message: '请输入用户名手机', 
-                            trigger: 'blur' 
-                        },
-                    ],
-                    num: [
-                        { 
-                            required: true, 
-                            message: '请输入验证码', 
-                            trigger: 'blur' 
-                        },
-                    ],
-                    username: [
-                        { 
-                            required: true, 
-                            message: '请输入名字', 
-                            trigger: 'blur' 
-                        },
-                    ],
-                    password: [
-                        { 
-                            required: true, 
-                            message: '请输入密码', 
-                            trigger: 'blur' 
-                        },
-                    ],
-                    confirmpassword: [
-                        { 
-                            required: true, 
-                            message: '请再次输入密码', 
-                            trigger: 'blur' 
-                        },
-                    ],
-                },
-            }
-        },
-    // 提交注册
+        }
+        return {
+            // 表单数据
+            form: {
+                username: "",   // 登录用户名/手机
+                password: "" ,   // 登录密码
+                checkPassword: "", // 确认密码
+                nickname: "",	// 昵称
+                captcha: ""		// 手机验证码
+            },
+            // 表单规则
+             rules: {
+                username: [{ 
+                    required: true, 
+                    message: '请输入用户名', 
+                    trigger: 'blur' 
+                }],
+                password: [{ 
+                    required: true, 
+                    message: '请输入密码', 
+                    trigger: 'blur' 
+                }],
+                checkPassword: [{ 
+                    validator: validatePass, 
+                    trigger: 'blur' 
+                }],
+                nickname: [{ 
+                    required: true, 
+                    message: '请输入昵称', 
+                    trigger: 'blur' 
+                }],
+                captcha: [{ 
+                    required: true, 
+                    message: '请输入验证码', 
+                    trigger: 'blur' 
+                }],
+            },
+        }
+    },
     methods: {
         // 发送验证码
-        handleSendNum(){
+        handleSendCaptcha(){
             if(!this.form.username){
                 this.$confirm('手机号码不能为空', '提示', {
                     confirmButtonText: '确定',
@@ -132,6 +117,7 @@ export default {
                 })
                 return;
             }
+
             if(this.form.username.length !== 11){
                 this.$confirm('手机号码格式错误', '提示', {
                     confirmButtonText: '确定',
@@ -140,6 +126,7 @@ export default {
                 })
                 return;
             }
+
             this.$axios({
                 url: `captchas`,
                 method: "POST",
@@ -156,23 +143,23 @@ export default {
             })
         },
 
-        handleRegisterSubmit(){
-            // 验证表单
+        // 注册
+        handleRegSubmit(){
             this.$refs['form'].validate((valid) => {
-                // 为true表示没有错误
                 if (valid) {
                     // 注册提交
-                    const {confirmpassword, ...props} = this.regForm;
+                    const {checkPassword, ...props} = this.regForm;
+
                     this.$axios({
-                        url: "/accounts/register",
+                        url: `/accounts/register`,
                         method: "POST",
                         data: props
                     }).then(res => {
                         console.log(res.data);
                     })
-                }
-            })
-        }
+                } 
+            });
+        },
     }
 }
 </script>
